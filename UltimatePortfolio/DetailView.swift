a56747fa454c75363091718576a03a3f337ef43f
Lochoa11 <linochoa11690@gmail.com>
2025-04-06 19:11:31 -0400

    adding code from day 2

---
+++ UltimatePortfolio/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  UltimatePortfolio
+//
+//  Created by Lin Ochoa on 3/27/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
